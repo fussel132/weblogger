64b6871fa426345d01c8efa1eff885b2ad77e368
Fussel132 <lukaskrome@gmail.com>
2023-01-19 23:40:09 +0100

    Fix 404

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ const port = 3000
 // Middleware
 app.use(bodyParser.json());
 app.use('/api', api)
-app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.status(200)
   res.set('Content-Type', 'text/html')
   res.sendFile(__dirname + '/public/index.html')
-  console.log(`Sent index.html to ${req.ip}`)
+  console.log(`Sent index.html to ${req.ip}. Reason: ${req.method} ${req.headers.host}${req.url}`)
 })
 
+app.use(express.static('public'))
+
 //The 404 Route (ALWAYS Keep this as the last route)
-app.get('*', function (req, res) {
+app.get('*', function (req, res, next) {
   res.status(404)
   res.set('Content-Type', 'text/html')
   res.sendFile(__dirname + '/public/404.html');
-  console.log(`Sent 404.html to ${req.ip}`)
+  console.log(`Sent 404.html to ${req.ip}. Reason: ${req.method} ${req.headers.host}${req.url}`)
 });
 
 app.listen(port, () => {
