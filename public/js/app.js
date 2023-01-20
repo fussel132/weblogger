be934a76bf4b9bd1ecacbc8ba0f179b0420a969d
Fussel132 <lukaskrome@gmail.com>
2023-01-20 23:35:19 +0100

    Implement Frontend TODO: - Add keep-alive connection to update if connected - Add line numbers/styling to the textarea - Fix css (static for now, needs to be adaptive)

---
+++ public/js/app.js
@@ -0,0 +1,94 @@
+let connectionState = "disconnected";
+let autoReconnect = false;
+
+function loadLog() {
+    let lines = document.getElementById('linesToLoad').value;
+    if (lines > 100 || lines < 1) {
+        // Can still be manipulated by the user, move this to server side
+        alert("Please enter a number between 1 and 100")
+    }
+    else {
+        document.getElementById('log').innerHTML = `Loaded ${lines} lines from the Server:\n`;
+        fetch('/api', {
+            method: 'GET',
+            headers: {
+                'reason': 'fetchLog',
+                'lines': lines
+            }
+        })
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('log').innerHTML += data;
+            })
+        // Get the events.log content from the server
+
+        /*
+        for (i = 1; i <= lines; i++) {
+            document.getElementById('log').innerHTML += `\nLine ${i} `;
+        }
+        */
+        // scroll to bottom
+        document.getElementById('log').scrollTop = document.getElementById('log').scrollHeight;
+    }
+}
+
+// Is called by the user if there is no connection
+function reconnect() {
+    if (connectionState == "disconnected") {
+        // TODO: Re-establish keep-alive connection
+        console.log("Reconnecting...") // Remove this in production, only for debugging purposes
+        loadLog()
+        setConnectionState("Connected");
+    }
+    else {
+        console.log("Cannot reconnect, connection is already established") // Remove this in production, only for debugging purposes
+    }
+}
+
+// Needs to be called if the keep-alive connection is lost
+function lostConnection() {
+    if (connectionState == "connected") {
+        setConnectionState("Disconnected");
+        if (autoReconnect) {
+            reconnect();
+        }
+    }
+}
+
+// Will be called if the user changes the auto-reconnect checkbox
+function setAutoReconnect() {
+    autoReconnect = document.getElementById('autoReconnect').checked;
+    console.log(`Auto-Reconnect is now ${autoReconnect}`) // Remove this in production, only for debugging purposes
+}
+
+function setConnectionState(state) {
+    if (state.toLowerCase() == "connected") {
+        connectionState = "connected";
+        document.getElementById('state').innerHTML = "Connected";
+        document.getElementById('state').style.backgroundColor = "#4CAF50";
+        document.getElementById('state').style.cursor = "default";
+    }
+    else if (state.toLowerCase() == "disconnected") {
+        connectionState = "disconnected";
+        document.getElementById('state').innerHTML = "Disconnected";
+        document.getElementById('state').style.backgroundColor = "#aa0404";
+        document.getElementById('state').style.cursor = "pointer";
+    }
+    else {
+        document.getElementById('state').innerHTML = "Unknown";
+    }
+}
+
+function showTime() {
+    const date = new Date();
+    let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+    let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+    document.getElementById('time').innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+// What should happen if the page finished loading?
+window.onload = () => {
+    setInterval(showTime, 500);
+    loadLog() // reconnect()
+}
--- server/api.js
+++ server/api.js
@@ -1,14 +1,31 @@
 const express = require('express')
-const fs = require('fs');
+const fs = require('fs')
+const readLastLines = require('read-last-lines')
 
 const eventLog = fs.createWriteStream(__dirname + '/events.log', { flags: 'a+' })
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.status(405)
-    res.set('Content-Type', 'text/html')
-    res.send("The API is not meant to be accessed with your browser (GET). Return to the <a href='/'>home page</a>.")
-    console.log(`Sent API Error to ${req.ip}`)
+    if (req.headers.reason == "fetchLog") {
+        let lines = req.headers.lines
+        res.status(200)
+        res.set('Content-Type', 'text/plain')
+        readLastLines.read(__dirname + '/events.log', lines)
+            .then((result) => {
+                res.send(result)
+            })
+        console.log(`Sent events.log (last ${lines} lines) to ${req.ip}. Reason: ${req.headers.reason}`)
+    }
+    else {
+        /*
+        res.status(405)
+        res.set('Content-Type', 'text/html')
+        res.send("The API is not meant to be accessed with your browser (GET). Return to the <a href='/'>home page</a>.")
+        console.log(`Sent API Error to ${req.ip}`)
+        */
+        next()
+    }
+
 })
 
 router.post('/', (req, res, next) => {
